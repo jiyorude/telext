f236f8d084d541403dcd70dde7013d9f37c8ca48
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-02-06 16:02:23 +0100

    06/02 16:02
    design commit

--- telexreact/src/WorldClock.js
+++ telexreact/src/WorldClock.js
@@ -2,8 +2,37 @@ import React from 'react'
 
 const WorldClock = () => {
   return (
-    <h1>WorldClock</h1>
-  )
+    <div className="backgroundwrapper">
+      <div className="flexwrapper">
+        <header>
+          <div className="row">
+            <div className="teltexttop">
+              <span className="regularspan">P694</span>
+              <span className="emptyspan"></span>
+              <span className="regularspan">APPOV</span>
+              <span className="emptyspan"></span>
+              <span className="regularspan">694</span>
+              <span className="longemptyspan"></span>
+              <span className="datespan">MON 23 JAN</span>
+              <span className="emptyspan"></span>
+              <span className="timespan">11:49:00</span>
+            </div>
+          </div>
+          <div className="rowTwo">
+            <div className="headerblock">
+                <h1>WORLDCLOCK</h1>
+            </div>
+          </div>
+          <div className="contentblock">
+            <div className="citiesleft"></div>
+            <div className="timeleft"></div>
+            <div className="citiesright"></div>
+            <div className="timeright"></div>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
 }
 
 export default WorldClock
