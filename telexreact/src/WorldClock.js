b8c605887027608573142b6ea3a095c03aeaf191
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-02-06 16:15:08 +0100

    06/02
    text commit

--- telexreact/src/WorldClock.js
+++ telexreact/src/WorldClock.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const WorldClock = () => {
   return (
@@ -20,19 +20,103 @@ const WorldClock = () => {
           </div>
           <div className="rowTwo">
             <div className="headerblock">
-                <h1>WORLDCLOCK</h1>
+              <h1>WORLDCLOCK</h1>
             </div>
           </div>
           <div className="contentblock">
-            <div className="citiesleft"></div>
-            <div className="timeleft"></div>
-            <div className="citiesright"></div>
-            <div className="timeright"></div>
+            <div className="citiesleft">
+              <div className="txtyellow">Amsterdam</div>
+              <div className="txtwhite">Honolulu</div>
+              <div className="txtwhite">Taipei</div>
+              <div className="txtwhite">Istanbul</div>
+              <div className="txtwhite">Nairobi</div>
+              <div className="txtwhite">Auckland</div>
+              <div className="txtwhite">Kyiv</div>
+              <div className="txtwhite">Rio de Janeiro</div>
+              <div className="txtwhite">Johannesburg</div>
+              <div className="txtwhite">Addis Ababa</div>
+              <div className="txtwhite">Singapore</div>
+              <div className="txtyellow">LONDON/GMT</div>
+              <div className="txtwhite">Jakarta</div>
+              <div className="txtwhite">Moscow</div>
+              <div className="txtwhite">Los Angeles</div>
+              <div className="txtwhite">Washington DC</div>
+              <div className="txtwhite">Bangkok</div>
+              <div className="txtwhite">Jerusalem</div>
+              <div className="txtwhite">Dubai</div>
+              <div className="txtwhite">Lisbon</div>
+            </div>
+            <div className="timeleft">
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+            </div>
+            <div className="citiesright">
+              <div className="txtwhite">Rome</div>
+              <div className="txtwhite">Edmonton</div>
+              <div className="txtwhite">Mexico City</div>
+              <div className="txtwhite">Beijing</div>
+              <div className="txtwhite">Pago Pago</div>
+              <div className="txtwhite">Melbourne</div>
+              <div className="txtwhite">New Delhi</div>
+              <div className="txtwhite">Helsinki</div>
+              <div className="txtwhite">Reykjavik</div>
+              <div className="txtwhite">Paris</div>
+              <div className="txtwhite">Baghdad</div>
+              <div className="txtwhite">Buenos Aires</div>
+              <div className="txtwhite">Hong Kong</div>
+              <div className="txtwhite">Shanghai</div>
+              <div className="txtwhite">Tokyo</div>
+              <div className="txtwhite">Brisbane</div>
+              <div className="txtwhite">Kuala Lumpur</div>
+              <div className="txtwhite">Istanbul</div>
+              <div className="txtwhite">Seoul</div>
+              <div className="txtwhite">Sydney</div>
+            </div>
+            <div className="timeright">
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+              <div className="time">00:00</div>
+            </div>
           </div>
         </header>
       </div>
     </div>
   );
-}
+};
 
-export default WorldClock
+export default WorldClock;
